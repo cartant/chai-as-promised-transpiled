fb4756905920a457739010dbe73f54b0fc67b280
Nicholas Jamieson <nicholas@cartant.com>
2017-07-16 11:13:23 +1000

    chore(build): Rename the typings module.

--- scripts/typings.js
+++ scripts/typings.js
@@ -3,5 +3,5 @@
 const fs = require("fs");
 
 let content = fs.readFileSync("./node_modules/@types/chai-as-promised/index.d.ts").toString();
-content = content.replace(/declare module 'chai-as-promised'/, "declare module '@cartant/chai-as-promised'");
+content = content.replace(/declare module 'chai-as-promised'/, "declare module 'chai-as-promised-transpiled'");
 fs.writeFileSync("./dist/index.d.ts", content);
